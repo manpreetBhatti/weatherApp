8fec83e284cd2a5e2afcd2c9b683e157e20cd0dc
Manpreet Bhatti <manpreet@axemneuro.com>
2024-04-08 11:31:55 -0400

    Adding Daily and Hourly Forecast API

--- src/App.js
+++ src/App.js
@@ -20,17 +20,18 @@ function App() {
   const [hourlyData, setHourlyData] = useState({});
   const [currentData, setCurrentData] = useState({});
   const [loader, setloader] = useState(true);
+  const url = "https://api.open-meteo.com/v1/forecast";
 
-  useState(() => {
+  useEffect(() => {
+    setloader(true);
     const params = {
       latitude: citySelected.lat,
       longitude: citySelected.lng,
       hourly: "temperature_2m",
       daily: ["temperature_2m_max", "sunrise", "sunset", "temperature_2m_min"],
       current: ["temperature_2m", "relative_humidity_2m", "wind_speed_10m"],
-      timezone: "America/New_York",
+      timezone: "America/Toronto",
     };
-    const url = "https://api.open-meteo.com/v1/forecast";
     axios
       .get(url, {
         params: params,
@@ -44,7 +45,7 @@ function App() {
         });
         setloader(false);
       });
-  }, []);
+  }, [citySelected]);
 
   return (
     <div>
@@ -64,8 +65,19 @@ function App() {
       ) : (
         <div className="App">
           <div className="cityNav">
-            {cities.map((e) => {
-              return <div className="cityName">{e.name}</div>;
+            {cities.map((e, index) => {
+              return (
+                <div
+                  className="cityName"
+                  key={index}
+                  onClick={() => {
+                    console.log(e);
+                    setCitySelected(e);
+                  }}
+                >
+                  {e.name}
+                </div>
+              );
             })}
           </div>
           <div className="searchDiv">
@@ -80,19 +92,18 @@ function App() {
             >
               <g
                 fill="#ffffff"
-                fill-rule="nonzero"
+                fillRule="nonzero"
                 stroke="none"
-                stroke-width="1"
-                stroke-linecap="butt"
-                stroke-linejoin="miter"
-                stroke-miterlimit="10"
-                stroke-dasharray=""
-                stroke-dashoffset="0"
-                font-family="none"
-                font-weight="none"
-                font-size="none"
-                text-anchor="none"
-                style={{ "mix-blend-mode": "normal" }}
+                strokeWidth="1"
+                strokeLinecap="butt"
+                strokeLinejoin="miter"
+                strokeMiterlimit="10"
+                strokeDasharray=""
+                strokeDashoffset="0"
+                fontFamily="none"
+                fontWeight="none"
+                fontSize="none"
+                textAnchor="none"
               >
                 <g transform="scale(5.33333,5.33333)">
                   <path d="M20.5,6c-7.99037,0 -14.5,6.50964 -14.5,14.5c0,7.99036 6.50963,14.5 14.5,14.5c3.45636,0 6.63371,-1.22096 9.12891,-3.25l9.81055,9.81055c0.37623,0.39185 0.9349,0.54969 1.46055,0.41265c0.52565,-0.13704 0.93616,-0.54754 1.07319,-1.07319c0.13704,-0.52565 -0.0208,-1.08432 -0.41265,-1.46055l-9.81055,-9.81055c2.02904,-2.4952 3.25,-5.67255 3.25,-9.12891c0,-7.99036 -6.50963,-14.5 -14.5,-14.5zM20.5,9c6.36905,0 11.5,5.13096 11.5,11.5c0,3.10261 -1.2238,5.90572 -3.20898,7.9707c-0.12237,0.08994 -0.23037,0.19794 -0.32031,0.32031c-2.06499,1.98518 -4.86809,3.20898 -7.9707,3.20898c-6.36905,0 -11.5,-5.13096 -11.5,-11.5c0,-6.36904 5.13095,-11.5 11.5,-11.5z"></path>
@@ -156,11 +167,45 @@ function App() {
           </div>
           <div className="hourlyDivContainer">
             <div className="title">HOURLY FORECAST</div>
-            <div></div>
+            <div className="hourlyTempDiv">
+              {hourlyData.time
+                .filter(
+                  (time) =>
+                    new Date().toDateString() === new Date(time).toDateString()
+                )
+                .filter(
+                  (time) => new Date(time).getHours() >= new Date().getHours()
+                )
+                .map((e, index) => {
+                  return (
+                    <div className="hourlyTemp" key={index}>
+                      <div>
+                        {new Date(e).toLocaleTimeString("en-US", {
+                          hour12: true,
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </div>
+                      <div>{hourlyData.temperature_2m[index]}&deg;C</div>
+                    </div>
+                  );
+                })}
+            </div>
           </div>
           <div className="dailyDivContainer">
             <div className="title">DAILY FORECAST</div>
-            <div></div>
+            <div className="dailyTempContainer">
+              {dailyData.temperature_2m_max.map((e, index) => {
+                return (
+                  <div className="dailyTempDiv" key={index}>
+                    <div>
+                      {new Date(dailyData.time[index]).toDateString("en-us")}
+                    </div>
+                    <div>{e}&deg;C</div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       )}
